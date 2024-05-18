9b359a0bcdaab11b229318d1d9b43c3926079240
Nexesenex <124105151+Nexesenex@users.noreply.github.com>
2024-05-18 20:16:53 +0200

    version 1.66g and updated cuda build batches

--- koboldcpp.py
+++ koboldcpp.py
@@ -728,6 +728,7 @@ requestsinqueue = 0
 defaultport = 5001
 KcppVersion = "1.68V"
 LcppVersion = "b3235"
+ReleaseDate = "2024/05/28"
 showdebug = True
 showsamplerwarning = True
 showmaxctxwarning = True
@@ -1205,7 +1206,7 @@ Enter Prompt:<br>
 
     def do_GET(self):
         global embedded_kailite, embedded_kcpp_docs, embedded_kcpp_sdui
-        global maxctx, maxhordelen, friendlymodelname, KcppVersion, LcppVersion, totalgens, preloaded_story, exitcounter, currentusergenkey, friendlysdmodelname, fullsdmodelpath, mmprojpath, password, fullwhispermodelpath
+        global maxctx, maxhordelen, friendlymodelname, KcppVersion, LcppVersion, ReleaseDate, totalgens, preloaded_story, exitcounter, currentusergenkey, friendlysdmodelname, fullsdmodelpath, mmprojpath, password, fullwhispermodelpath
         self.path = self.path.rstrip('/')
         response_body = None
         content_type = 'application/json'
@@ -3613,7 +3614,7 @@ def main(launch_args,start_server=True):
         s_pp = float(benchmaxctx-benchlen)/t_pp
         s_gen = float(benchlen)/t_gen
         datetimestamp = datetime.now(timezone.utc)
-        print(f"\nBenchmark Completed - v{KcppVersion} based on LlamaCPP {LcppVersion} Results:\n======")
+        print(f"\nBenchmark Completed - v{KcppVersion} based on LlamaCPP {LcppVersion}; Release date: {ReleaseDate} Results:\n======")
         print(f"Timestamp: {datetimestamp}")
         print(f"Backend: {libname}")
         print(f"Model: {benchmodel}")
@@ -3637,7 +3638,7 @@ def main(launch_args,start_server=True):
                     file.seek(0, 2)
                     if file.tell() == 0: #empty file
                         file.write(f"Datime,KCPP,LCPP,Backend,Model,Threads,Layers,BlasThreads,BBSize,FlashA,MaxCtx,GenNum,PPTime,PPSpeed,TGTime,TGSpeed,TotalTime,Coherence,Output")
-                    file.write(f"\n{datetimestamp},{KcppVersion},{LcppVersion},{libname},{benchmodel},{args.threads},{args.gpulayers},{args.blasthreads},{args.blasbatchsize},{args.flashattention},{benchmaxctx},{benchlen},{t_pp:.2f},{s_pp:.2f},{t_gen:.2f},{s_gen:.2f},{(t_pp+t_gen):.2f},{resultok},{result}")
+                    file.write(f"\n{ReleaseDate},{KcppVersion},{LcppVersion},{libname},{benchmodel},{args.threads},{args.gpulayers},{args.blasthreads},{args.blasbatchsize},{args.flashattention},{benchmaxctx},{benchlen},{t_pp:.2f},{s_pp:.2f},{t_gen:.2f},{s_gen:.2f},{(t_pp+t_gen):.2f},{resultok},{result}")
             except Exception as e:
                 print(f"Error writing benchmark to file: {e}")
         global using_gui_launcher
@@ -3694,7 +3695,8 @@ if __name__ == '__main__':
         return range_checker
 
     print("***\nWelcome to KoboldCpp Frankenstein Fork - Version " + KcppVersion) # just update version manually
-    print("***\nBased on LlamaCpp - Version " + LcppVersion) # just update version manually
+    print("***\nBased on LlamaCpp - Version " + LcppVersion) # just update LlamaCPP version manually
+    print("***\nRelease date: " + ReleaseDate) # just update date manually
     print("***")
     # print("Python version: " + sys.version)
     parser = argparse.ArgumentParser(description='KoboldCpp Server')
