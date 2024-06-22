49b3c613b6ae7e3557b9f5556886f499e69bb8bd
Nexesenex <124105151+Nexesenex@users.noreply.github.com>
2024-06-22 05:12:13 +0200

    Gradient rope formula with offsets
    Positive for Solar models
    Negative for Llama 1 and 2 models

--- gpttype_adapter.cpp
+++ gpttype_adapter.cpp
@@ -798,10 +798,44 @@ static float CalcGradientAIRopeFreqBase(float original_rope_base, int n_ctx_trai
     {
         return original_rope_base;
     }
-    float ctx_multiplier = (is_solar?8.0f:1.0f);
-	float chi_ctx_train_value = (n_ctx_train * ctx_multiplier) / 6.28318;
-    float chi_ctx_value = (n_ctx_desired * ctx_multiplier) / 6.28318;
-    return powf(original_rope_base, logf(chi_ctx_value) / logf(chi_ctx_train_value));
+	else
+	{
+        float ctx_multiplier = (is_solar?8.0f:1.0f);
+        float chi_ctx_train_value = (n_ctx_train * ctx_multiplier) / 6.28318;
+        float chi_ctx_value = (n_ctx_desired * ctx_multiplier) / 6.28318;
+        float gradient_ai_rope_freq_base_value = powf(original_rope_base, log10f(chi_ctx_value) / log10f(chi_ctx_train_value));
+
+        printf("Trained max context length (value:%.d).\n", n_ctx_train);
+        printf("Desired context length (value:%.d).\n", n_ctx_desired);
+        printf("Solar context multiplier (value:%.3f).\n", ctx_multiplier);
+        printf("Chi context train (value:%.3f).\n", chi_ctx_train_value);
+        printf("Chi chosen context (value:%.3f).\n", chi_ctx_value);
+        printf("Log Chi context train (value:%.3f).\n", log10f(chi_ctx_train_value));
+        printf("Log Chi chosen context (value:%.3f).\n", log10f(chi_ctx_value));
+	    printf("RoPE Frequency Base value (value:%.d).\n", original_rope_base);	
+        printf("RoPE base calculated via Gradient AI formula. (value:%.1f).\n", gradient_ai_rope_freq_base_value);
+
+	    if(original_rope_base==10000.0f && (file_format_meta.n_tensors==435 || file_format_meta.n_tensors==611))
+        {
+            float extended_rope_positive_offset_value = 1 + ((log10f(chi_ctx_value) - log10f(chi_ctx_train_value)) / ((log10f(chi_ctx_value) * log10f(chi_ctx_train_value)) - (log10f(chi_ctx_value) + log10f(chi_ctx_train_value))));
+            printf("Extended Rope Positive Offset (multiplicator for Solar based models. (value:%.3f).\n", extended_rope_positive_offset_value);
+            float rope_freq_base_with_positive_offset = gradient_ai_rope_freq_base_value * extended_rope_positive_offset_value;
+            printf("RoPE base calculated via Gradient AI formula for Solar based models. (value:%.1f).\n", rope_freq_base_with_positive_offset);
+            return rope_freq_base_with_positive_offset;
+        }
+	    else if(original_rope_base==10000.0f)
+        {
+            float extended_rope_negative_offset_value = 1 + ((log10f(chi_ctx_value) - log10f(chi_ctx_train_value)) / (3.14159265358979323846 * 3.14159265358979323846));
+            printf("Extended Rope Negative Offset (divisor) for Llama 1 and 2 based models. (value:%.3f).\n", extended_rope_negative_offset_value);
+            float rope_freq_base_with_negative_offset = gradient_ai_rope_freq_base_value / extended_rope_negative_offset_value;		
+            printf("RoPE base calculated via Gradient AI formula for Llama 1 and 2 based models. (value:%.1f).\n", rope_freq_base_with_negative_offset);
+            return rope_freq_base_with_negative_offset;
+        }
+        else
+        {
+	        return gradient_ai_rope_freq_base_value;
+        }
+    }
 }
 
 ModelLoadResult gpttype_load_model(const load_model_inputs inputs, FileFormat in_file_format, FileFormatExtraMeta in_file_format_meta)
@@ -859,7 +893,8 @@ ModelLoadResult gpttype_load_model(const load_model_inputs inputs, FileFormat in
         rope_freq_base = CalcGradientAIRopeFreqBase(10000.0f,2048,kcpp_params->n_ctx,false);
         if(file_format==FileFormat::GGUF_GENERIC)
         {
-            printf("Using automatic RoPE scaling. If the model has customized RoPE settings, they will be used directly instead!\n");
+            printf("Using automatic RoPE scaling for GGUF. If the model has custom RoPE settings, they'll be used directly instead!\n");
+            printf("It means that the RoPE values written above will be replaced by the RoPE values indicated after loading.\n");
         }
         else
         {
