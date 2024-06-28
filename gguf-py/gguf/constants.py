95b157f9043456b6509643dcde261a7f286eed55
Andrei Betlen <abetlen@gmail.com>
2024-06-28 15:42:19 -0400

    Gemma v2 second PR 8 commits
    Add attention and final logit softcapping.
    
    Add custom add_ functions
    
    Disable flash attention for Gemma2
    
    Merge branch 'master' of github.com:ggerganov/llama.cpp into add-gemma2-soft-capping
    
    Add default value for attention and final logit softcap value
    
    Merge branch 'add-gemma2-soft-capping' of github.com:ggerganov/llama.cpp into add-gemma2-soft-capping
    
    Add custom kq scaling from Gemma2Attention

--- gguf-py/gguf/constants.py
+++ gguf-py/gguf/constants.py
@@ -50,6 +50,9 @@ class Keys:
         POOLING_TYPE                      = "{arch}.pooling_type"
         LOGIT_SCALE                       = "{arch}.logit_scale"
         DECODER_START_TOKEN_ID            = "{arch}.decoder_start_token_id"
+        ATTN_LOGIT_SOFTCAPPING            = "{arch}.attn_logit_softcapping"
+        FINAL_LOGIT_SOFTCAPPING           = "{arch}.final_logit_softcapping"
+        QUERY_PRE_ATTN_SCALAR             = "{arch}.query_pre_attn_scalar"
 
     class Attention:
         HEAD_COUNT        = "{arch}.attention.head_count"
--- gguf-py/gguf/gguf_writer.py
+++ gguf-py/gguf/gguf_writer.py
@@ -516,6 +516,15 @@ class GGUFWriter:
     def add_logit_scale(self, value: float) -> None:
         self.add_float32(Keys.LLM.LOGIT_SCALE.format(arch=self.arch), value)
 
+    def add_attn_logit_softcapping(self, value: float) -> None:
+        self.add_float32(Keys.LLM.ATTN_LOGIT_SOFTCAPPING.format(arch=self.arch), value)
+
+    def add_final_logit_softcapping(self, value: float) -> None:
+        self.add_float32(Keys.LLM.FINAL_LOGIT_SOFTCAPPING.format(arch=self.arch), value)
+
+    def add_query_pre_attn_scalar(self, value: float) -> None:
+        self.add_float32(Keys.LLM.QUERY_PRE_ATTN_SCALAR.format(arch=self.arch), value)
+
     def add_expert_count(self, count: int) -> None:
         self.add_uint32(Keys.LLM.EXPERT_COUNT.format(arch=self.arch), count)
 
