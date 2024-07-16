ba5319be051a46deef52e67015076249c11b0236
Nexesenex <124105151+Nexesenex@users.noreply.github.com>
2024-07-16 14:23:22 +0200

    Update llama.cpp

--- src/llama.cpp
+++ src/llama.cpp
@@ -19357,47 +19357,47 @@ static ggml_type llama_tensor_get_type(quantize_state_internal & qs, ggml_type n
         }
     } else if (ftype == LLAMA_FTYPE_MOSTLY_IQ4_LR) {
         if (name.find("attn_q.weight") != std::string::npos) {
-            new_type = GGML_TYPE_IQ3_S;
+            new_type = GGML_TYPE_IQ3_XXS;
         }
         else if (name.find("attn_k.weight") != std::string::npos) {
             if (qs.model.hparams.n_expert >= 2) new_type = GGML_TYPE_Q5_K;
-            else if (qs.model.hparams.n_gqa() >= 2) new_type = GGML_TYPE_Q5_K;
+            else if (qs.model.hparams.n_gqa() >= 2) new_type = GGML_TYPE_IQ4_XS;
             else new_type = GGML_TYPE_IQ4_XS;
         }
         else if (name.find("attn_v.weight") != std::string::npos) {
             if (qs.model.hparams.n_expert >= 2) new_type = GGML_TYPE_Q6_K;
-            else if (qs.model.hparams.n_gqa() >= 4) new_type = GGML_TYPE_Q6_K;
+            else if (qs.model.hparams.n_gqa() >= 4) new_type = GGML_TYPE_Q5_K;
             else new_type = GGML_TYPE_Q6_K;
             ++qs.i_attention_wv;
         }
         else if (name.find("attn_output.weight") != std::string::npos) {
             if (qs.model.hparams.n_expert >= 2) new_type = GGML_TYPE_Q6_K;
-            else new_type = GGML_TYPE_Q5_K;
+            else new_type = GGML_TYPE_IQ4_XS;
         }
         else if (name.find("ffn_gate") != std::string::npos) {
-            if (qs.i_ffn_gate < qs.n_ffn_gate/3) {
+            if (qs.i_ffn_gate < qs.n_ffn_gate/1) {
                 new_type = ftype == LLAMA_FTYPE_MOSTLY_IQ4_LR ?
-                GGML_TYPE_Q5_K : GGML_TYPE_IQ4_XS;
+                GGML_TYPE_IQ4_XS : GGML_TYPE_IQ3_XXS;
             }
             ++qs.i_ffn_gate;
         }
         else if (name.find("ffn_down") != std::string::npos) {
             auto info = layer_info(qs.i_ffn_down, qs.n_ffn_down, name.c_str());
             int i_layer = info.first, n_layer = info.second;
-            new_type = i_layer < n_layer/32 ? GGML_TYPE_Q5_K : use_more_bits(i_layer, n_layer) ?
-            GGML_TYPE_Q5_K : GGML_TYPE_IQ4_XS;
+            new_type = i_layer < n_layer/32 ? GGML_TYPE_IQ4_XS : use_more_bits(i_layer, n_layer) ?
+            GGML_TYPE_IQ4_XS : GGML_TYPE_IQ3_XXS;
             ++qs.i_ffn_down;	
         }
         else if (name.find("ffn_up") != std::string::npos) {
-            if (qs.i_ffn_up < qs.n_ffn_up/3) {
+            if (qs.i_ffn_up < qs.n_ffn_up/1) {
                 new_type = ftype == LLAMA_FTYPE_MOSTLY_IQ4_LR ?
-                GGML_TYPE_Q5_K : GGML_TYPE_IQ4_XS;
+                GGML_TYPE_IQ4_XS : GGML_TYPE_IQ3_XXS;
             }
             ++qs.i_ffn_up;
         }
     } else if (ftype == LLAMA_FTYPE_MOSTLY_IQ4_BLR) {
         if (name.find("attn_q.weight") != std::string::npos) {
-            new_type = GGML_TYPE_IQ4_XS;
+            new_type = GGML_TYPE_IQ3_XXS;
         }
         else if (name.find("attn_k.weight") != std::string::npos) {
             if (qs.model.hparams.n_expert >= 2) new_type = GGML_TYPE_Q5_K;
@@ -20469,8 +20469,8 @@ static void llama_model_quantize_internal(const std::string & fname_inp, const s
         case LLAMA_FTYPE_MOSTLY_IQ4_XSR: default_type = GGML_TYPE_IQ3_S;   break;
         case LLAMA_FTYPE_MOSTLY_IQ4_SR:  default_type = GGML_TYPE_IQ3_S;   break;
         case LLAMA_FTYPE_MOSTLY_IQ4_MR:  default_type = GGML_TYPE_IQ3_S;   break;
-        case LLAMA_FTYPE_MOSTLY_IQ4_LR:  default_type = GGML_TYPE_IQ4_XS;  break;
-        case LLAMA_FTYPE_MOSTLY_IQ4_BLR: default_type = GGML_TYPE_IQ4_XS;  break;
+        case LLAMA_FTYPE_MOSTLY_IQ4_LR:  default_type = GGML_TYPE_IQ3_XXS;  break;
+        case LLAMA_FTYPE_MOSTLY_IQ4_BLR: default_type = GGML_TYPE_IQ3_XXS;  break;
 
         // Nexesenex test IQ quants
         case LLAMA_FTYPE_MOSTLY_IQ5_XSR: default_type = GGML_TYPE_IQ4_XS;  break;
