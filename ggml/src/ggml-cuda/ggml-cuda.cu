38d0b5e4d9f3601396b08b430ab5904864e578fd
Nexesenex <124105151+Nexesenex@users.noreply.github.com>
2024-12-14 19:40:49 +0100

    Allow bf16 kv-cache #69
    Copyright @ Iwan Kawrakow @ikawrakow
    
    Might not "really" work, Ikawrakow says he might have missed a conversion, so it's FP16 actually.
    Template left for later.

--- ggml/src/ggml-cuda/ggml-cuda.cu
+++ ggml/src/ggml-cuda/ggml-cuda.cu
@@ -2967,6 +2967,9 @@ static bool ggml_backend_cuda_device_supports_op(ggml_backend_dev_t dev, const g
                 if (src0_type == GGML_TYPE_F32 && src1_type == GGML_TYPE_F16) {
                     return true;
                 }
+                if (src0_type == GGML_TYPE_F32 && src1_type == GGML_TYPE_BF16) {
+                    return true;
+                } // KV BF16 infers, but PPL same as KV F16 accordingly to IK.
                 if (src0_type == GGML_TYPE_F32 && src1_type == GGML_TYPE_Q8_0) {
                     return true;
                 }
