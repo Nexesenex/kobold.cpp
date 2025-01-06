a6e68c276cd678abf7d74a9e9bc17b54d96c6a04
Johannes Gäßler <johannesg@5d6.de>
2025-01-06 02:33:52 +0100

    CUDA: add BF16 support (#11093)
    * CUDA: add BF16 support

--- ggml/src/ggml-cuda/convert.cu
+++ ggml/src/ggml-cuda/convert.cu
@@ -1392,9 +1392,11 @@ to_fp32_cuda_t ggml_get_to_fp32_cuda(ggml_type type) {
         case GGML_TYPE_F16:
             return convert_unary_cuda<half>;
         // case GGML_TYPE_BF16:
-            // return convert_from_bf16_cuda;
+            // return convert_from_bf16_cuda; // IK
+        // case GGML_TYPE_BF16:
+            // return convert_unary_cuda<__nv_bfloat16>; // Jart
         case GGML_TYPE_BF16:
-            return convert_unary_cuda<__nv_bfloat16>;
+            return convert_unary_cuda<nv_bfloat16>;
         default:
             return nullptr;
     }
--- ggml/src/ggml-cuda/ggml-cuda.cu
+++ ggml/src/ggml-cuda/ggml-cuda.cu
@@ -1935,7 +1935,7 @@ static void ggml_cuda_mul_mat(ggml_backend_cuda_context & ctx, const ggml_tensor
         && src1->type == GGML_TYPE_F32 && dst->type == GGML_TYPE_F32
         && src0->ne[0] % (GGML_CUDA_DMMV_X*2) == 0 && src1->ne[1] == 1;
 
-    bool use_mul_mat_vec   = src0->type == GGML_TYPE_F16
+    bool use_mul_mat_vec   = (src0->type == GGML_TYPE_F16 || src0->type == GGML_TYPE_BF16)
         && src1->type == GGML_TYPE_F32 && dst->type == GGML_TYPE_F32
         && src0->ne[0] % 2 == 0 && src1->ne[1] == 1;
 
