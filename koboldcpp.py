59866473c503a3d004bb3f16b0be53367e855285
Nexesenex <124105151+Nexesenex@users.noreply.github.com>
2025-03-20 03:16:16 +0100

    v1.87001_b4927

--- koboldcpp.py
+++ koboldcpp.py
@@ -56,8 +56,8 @@ dry_seq_break_max = 512
 # dry_seq_break_max = 128
 
 # global vars
-KcppVersion = "1.87000"
-LcppVersion = "b4924"
+KcppVersion = "1.87001"
+LcppVersion = "b4927"
 CudaSpecifics = "Cu128_Ar86_SMC2_DmmvX32Y1"
 ReleaseDate = "2025/03/19"
 showdebug = True
