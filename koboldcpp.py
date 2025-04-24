24511fd9dce4faa50b0cd85708880fce8c08fda8
Nexesenex <124105151+Nexesenex@users.noreply.github.com>
2025-04-24 15:33:32 +0200

    v1.90001_b5176_RM181c6

--- koboldcpp.py
+++ koboldcpp.py
@@ -59,7 +59,7 @@ dry_seq_break_max = 512
 # dry_seq_break_max = 128
 
 # global vars
-KcppVersion = "1.90000"
+KcppVersion = "1.90001"
 LcppVersion = "b5176"
 EsoboldVersion = "RMv1.8.1+6c"
 CudaSpecifics = "Cu128_Ar86_SMC2_DmmvX32Y1"
