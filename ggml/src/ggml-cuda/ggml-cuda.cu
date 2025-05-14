9999fa292adba57fa6a9223f2f322c9d9df7dca8
Nexesenex <124105151+Nexesenex@users.noreply.github.com>
2025-05-14 22:50:38 +0200

    Allow q8_0 KV cache for head size 256 #330
    Co-Authored-By: Kawrakow <iwankawrakow@gmail.com>

--- ggml/src/ggml-cuda/ggml-cuda.cu
+++ ggml/src/ggml-cuda/ggml-cuda.cu
@@ -3529,6 +3529,11 @@ static bool ggml_backend_cuda_device_supports_op(ggml_backend_dev_t dev, const g
                 // different head sizes of K and V are not supported yet
                 return false;
             }
+            if (op->src[1]->ne[0] == 256 && op->src[2]->ne[0] == 256 &&
+               (op->src[1]->type == GGML_TYPE_F16 || op->src[1]->type == GGML_TYPE_Q8_0) &&
+               (op->src[2]->type == GGML_TYPE_F16 || op->src[2]->type == GGML_TYPE_Q8_0)) {
+                return true;
+            }
             if (op->src[0]->ne[0] == 192) {
                 return false;
             }
