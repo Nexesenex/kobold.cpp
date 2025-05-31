028350369e9daa0cb08df0646a1a8cf92bf106b7
Nexesenex <124105151+Nexesenex@users.noreply.github.com>
2025-05-31 14:02:35 +0200

    Revert "attempt to debug"
    This reverts commit 3829ae728e83b4d85d6c472900ed0b1f8a162401.

--- ggml/src/ggml-cuda/fattn-mma-f16.cuh
+++ ggml/src/ggml-cuda/fattn-mma-f16.cuh
@@ -871,10 +871,10 @@ static __global__ void flash_attn_ext_f16(
 	
 #if __CUDA_ARCH__ == GGML_CUDA_CC_TURING
     if (ncols1*ncols2 > 32) {
-        printf("\nCUDA_ARCH:%d, DKQ:%d, DV:%d, ncols1:%d, ncols2:%d, nwarps:%d, ntiles:%d, ne00:%d, ne01:%d\n",__CUDA_ARCH__,DKQ, DV, ncols1, ncols2, nwarps, ntiles,ne00,ne01);
         NO_DEVICE_CODE;
         return;
     }
+
 #endif // __CUDA_ARCH__ == GGML_CUDA_CC_TURING
 
     static_assert(FATTN_KQ_STRIDE % KQ_per_iter == 0, "bad KQ_per_iter");
