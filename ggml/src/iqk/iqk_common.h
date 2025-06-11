31e691183951dc650bb45fbba0d9e67f3d049398
Nexesenex <124105151+Nexesenex@users.noreply.github.com>
2025-06-11 19:21:34 +0200

    fix convergence

--- ggml/src/iqk/iqk_common.h
+++ ggml/src/iqk/iqk_common.h
@@ -243,7 +243,6 @@ static inline float hmax_float_8(__m256 x) {
     max4 = _mm_max_ss( max4, _mm_movehdup_ps( max4));
     return  _mm_cvtss_f32(max4);
 }
-
 static inline __m128 hsum_float_4x4(__m128 * accm) {
     accm[0] = _mm_add_ps(_mm_unpacklo_ps(accm[0], accm[2]), _mm_unpackhi_ps(accm[0], accm[2]));
     accm[1] = _mm_add_ps(_mm_unpacklo_ps(accm[1], accm[3]), _mm_unpackhi_ps(accm[1], accm[3]));
@@ -251,11 +250,9 @@ static inline __m128 hsum_float_4x4(__m128 * accm) {
 }
 static inline __m256 hsum_float_8x8(__m256 * accm) {
     for (int i = 0; i < 4; ++i) {
-        accm[i] = _mm256_add_ps(_mm256_permute2f128_ps(accm[i], accm[i+4], 0x20), _mm256_permute2f128_ps(accm[i], accm[i+4], 0x31));
-        //accm[i] = _mm256_set_m128(_mm_add_ps(_mm256_castps256_ps128(accm[i+4]), _mm256_extractf128_ps(accm[i+4], 1)), // old
+        accm[i] = _mm256_add_ps(_mm256_permute2f128_ps(accm[i], accm[i + 4], 0x20), _mm256_permute2f128_ps(accm[i], accm[i + 4], 0x31));
+        //accm[i] = _mm256_set_m128(_mm_add_ps(_mm256_castps256_ps128(accm[i+4]), _mm256_extractf128_ps(accm[i+4], 1)),
         //                          _mm_add_ps(_mm256_castps256_ps128(accm[i+0]), _mm256_extractf128_ps(accm[i+0], 1)));
-        //accm[i] = _mm256_set_m128(_mm_add_ps(_mm256_castps256_ps128(accm[i + 4]), _mm256_extractf128_ps(accm[i + 4], 1)), // new
-        //                                     _mm_add_ps(_mm256_castps256_ps128(accm[i + 0]), _mm256_extractf128_ps(accm[i + 0], 1)));
     }
     for (int i = 0; i < 2; ++i) accm[i] = _mm256_add_ps(_mm256_unpacklo_ps(accm[i], accm[i + 2]), _mm256_unpackhi_ps(accm[i], accm[i + 2]));
     return _mm256_add_ps(_mm256_unpacklo_ps(accm[0], accm[1]), _mm256_unpackhi_ps(accm[0], accm[1]));
