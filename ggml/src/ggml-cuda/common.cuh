652985be7401642e4858a4d0eacb6acc334feace
Nexesenex <124105151+Nexesenex@users.noreply.github.com>
2025-06-12 02:40:53 +0200

    New Trellis IQ2_KT, IQ4_KT.
    Including Cuda + Cuda MMQ.
    Credits : @Ikawrakow

--- ggml/src/ggml-cuda/common.cuh
+++ ggml/src/ggml-cuda/common.cuh
@@ -664,12 +664,12 @@ struct ggml_cuda_type_traits<GGML_TYPE_IQ2_KT> {
     // static constexpr int qi = QI4_XS;
 // };
 
-// template<>
-// struct ggml_cuda_type_traits<GGML_TYPE_IQ4_KT> {
-    // static constexpr int qk = QK_K;
-    // static constexpr int qr = QR4_XS;
-    // static constexpr int qi = QI4_XS;
-// };
+template<>
+struct ggml_cuda_type_traits<GGML_TYPE_IQ4_KT> {
+    static constexpr int qk = QK_K;
+    static constexpr int qr = QR4_XS;
+    static constexpr int qi = QI4_XS;
+};
 
 template<>
 struct ggml_cuda_type_traits<GGML_TYPE_IQ3_K> {
