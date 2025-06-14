1885c3c1eaadaa8f6688590c56444d9242cb3818
Nexesenex <124105151+Nexesenex@users.noreply.github.com>
2025-06-14 16:00:18 +0200

    Reapply "IK Softcap op"
    This reverts commit 1eb881b8397736a02ea633a2d4e8df421a355015.

--- ggml/src/ggml-alloc.c
+++ ggml/src/ggml-alloc.c
@@ -59,6 +59,7 @@ static bool ggml_op_can_inplace(enum ggml_op op) {
         case GGML_OP_RMS_NORM_BACK:
         case GGML_OP_SOFT_MAX:
         case GGML_OP_SOFT_MAX_BACK:
+        case GGML_OP_SOFTCAP:	
             return true;
 
         default:
