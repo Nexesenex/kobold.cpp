b6d8a6a92ec2bd0850b3a65aa0b9e24e4fd7b8f5
Nexesenex <124105151+Nexesenex@users.noreply.github.com>
2025-07-04 13:45:31 +0200

    Fix merge in respect for IKL's softcap/softmax.

--- ggml/src/ggml-cuda/softmax.cu
+++ ggml/src/ggml-cuda/softmax.cu
@@ -44,7 +44,8 @@ struct soft_max_params {
 #endif // __clang__
 template <bool use_shared, int ncols_template, int block_size_template, typename T>
 static __global__ void soft_max_f32(
-        const float * x, const T * mask, float * dst, const soft_max_params p, bool do_softcap) {
+        const float * x, const T * mask, float * dst, const soft_max_params p,
+        float cap_params0, float cap_params1, bool do_softcap) {
     const int ncols = ncols_template == 0 ? p.ncols : ncols_template;
 
     const int tid  = threadIdx.x;
@@ -87,7 +88,7 @@ static __global__ void soft_max_f32(
         }
 
         const int64_t ix = (int64_t)rowx*ncols + col;
-        const int64_t iy = (int64_t)rowy*ncols + col;
+        // const int64_t iy = (int64_t)rowy*ncols + col;
 
         // const float val = x[ix]*scale + (mask ? slope*t2f32(mask[iy]) : 0.0f);
 		
@@ -95,7 +96,7 @@ static __global__ void soft_max_f32(
 
         // const float val = x[col]*p.scale + (mask ? slope*t2f32(mask[col]) : 0.0f);
 		
-        const float val = do_softcap ? p.scale*cap_params1*tanhf(cap_params0*x[ix]) + (mask ? slope*t2f32(mask[iy]) : 0.0f) :
+        const float val = do_softcap ? p.scale*cap_params1*tanhf(cap_params0*x[ix]) + (mask ? slope*t2f32(mask[col]) : 0.0f) :
                                        x[col]*p.scale + (mask ? slope*t2f32(mask[col]) : 0.0f);		
 									  		   
         vals[col] = val;
@@ -192,7 +193,8 @@ static __global__ void soft_max_back_f32(
 }
 
 template<typename T>
-static void soft_max_f32_cuda(const float * x, const T * mask, float * dst, const soft_max_params & params, bool do_softcap, cudaStream_t stream) {
+static void soft_max_f32_cuda(const float * x, const T * mask, float * dst, const soft_max_params & params,
+        float cap_params0, float cap_params1, bool do_softcap, cudaStream_t stream) {
     int nth = WARP_SIZE;
     const int64_t ncols_x = params.ncols;
 
@@ -208,44 +210,45 @@ static void soft_max_f32_cuda(const float * x, const T * mask, float * dst, cons
         switch (ncols_x) {
             case 32:
                 soft_max_f32<true,   32,   32><<<block_nums, block_dims, nbytes_shared, stream>>>
-                    (x, mask, dst, params, do_softcap);
+                    (x, mask, dst, params, cap_params0, cap_params1, do_softcap);
                 break;
             case 64:
                 soft_max_f32<true,   64,   64><<<block_nums, block_dims, nbytes_shared, stream>>>
-                    (x, mask, dst, params, do_softcap);
+                    (x, mask, dst, params, cap_params0, cap_params1, do_softcap);
                 break;
             case 128:
                 soft_max_f32<true,  128,  128><<<block_nums, block_dims, nbytes_shared, stream>>>
-                    (x, mask, dst, params, do_softcap);
+                    (x, mask, dst, params, cap_params0, cap_params1, do_softcap);
                 break;
             case 256:
                 soft_max_f32<true,  256,  256><<<block_nums, block_dims, nbytes_shared, stream>>>
-                    (x, mask, dst, params, do_softcap);
+                    (x, mask, dst, params, cap_params0, cap_params1, do_softcap);
                 break;
             case 512:
                 soft_max_f32<true,  512,  512><<<block_nums, block_dims, nbytes_shared, stream>>>
-                    (x, mask, dst, params, do_softcap);
+                    (x, mask, dst, params, cap_params0, cap_params1, do_softcap);
                 break;
             case 1024:
                 soft_max_f32<true, 1024, 1024><<<block_nums, block_dims, nbytes_shared, stream>>>
-                    (x, mask, dst, params, do_softcap);
+                    (x, mask, dst, params, cap_params0, cap_params1, do_softcap);
                 break;
             case 2048:
                 soft_max_f32<true, 2048, 1024><<<block_nums, block_dims, nbytes_shared, stream>>>
-                    (x, mask, dst, params, do_softcap);
+                    (x, mask, dst, params, cap_params0, cap_params1, do_softcap);
                 break;
             case 4096:
                 soft_max_f32<true, 4096, 1024><<<block_nums, block_dims, nbytes_shared, stream>>>
-                    (x, mask, dst, params, do_softcap);
+                    (x, mask, dst, params, cap_params0, cap_params1, do_softcap);
                 break;
             default:
                 soft_max_f32<true,    0,    0><<<block_nums, block_dims, nbytes_shared, stream>>>
-                    (x, mask, dst, params, do_softcap);
+                    (x, mask, dst, params, cap_params0, cap_params1, do_softcap);
                 break;
         }
     } else {
         const size_t nbytes_shared_low = WARP_SIZE*sizeof(float);
-        soft_max_f32<false, 0, 0><<<block_nums, block_dims, nbytes_shared_low, stream>>>(x, mask, dst, params, do_softcap);
+        soft_max_f32<false, 0, 0><<<block_nums, block_dims, nbytes_shared_low, stream>>>(
+            x, mask, dst, params, cap_params0, cap_params1, do_softcap);
     }
 }
 
@@ -325,7 +328,7 @@ void ggml_cuda_op_soft_max(ggml_backend_cuda_context & ctx, ggml_tensor * dst) {
 
         // soft_max_f32_cuda(src0_d, (const half  *) src1_d, dst_d, ne00, nrows_x, nrows_y, scale, max_bias, 0, 0, false, stream);
 
-        soft_max_f32_cuda(src0_d, (const half  *) src1_d, dst_d, params, false, stream);
+        soft_max_f32_cuda(src0_d, (const half  *) src1_d, dst_d, params, 0, 0, false, stream);
 
     } else {
 
@@ -333,7 +336,7 @@ void ggml_cuda_op_soft_max(ggml_backend_cuda_context & ctx, ggml_tensor * dst) {
 
         // soft_max_f32_cuda(src0_d, (const float *) src1_d, dst_d, ne00, nrows_x, nrows_y, scale, max_bias, 0, 0, false, stream);
 
-        soft_max_f32_cuda(src0_d, (const float *) src1_d, dst_d, params, false, stream);
+        soft_max_f32_cuda(src0_d, (const float *) src1_d, dst_d, params, 0, 0, false, stream);
     }
 }
 
@@ -352,12 +355,52 @@ void ggml_cuda_op_soft_cap_max(ggml_backend_cuda_context & ctx, ggml_tensor * ds
 
     GGML_ASSERT(!src1 || src1->type == GGML_TYPE_F16 || src1->type == GGML_TYPE_F32); // src1 contains mask and it is optional
 
-    const int64_t ne00    = src0->ne[0];
     const int64_t nrows_x = ggml_nrows(src0);
     const int64_t nrows_y = src0->ne[1];
 
-    float params[4];
-    memcpy(params, dst->op_params, sizeof(params));
+    const int64_t ne00    = src0->ne[0];
+
+    float scale    = 1.0f;
+    float max_bias = 0.0f;
+
+    memcpy(&scale,    (const float *) dst->op_params + 0, sizeof(float));
+    memcpy(&max_bias, (const float *) dst->op_params + 1, sizeof(float));
+
+    const int64_t nb11 = src1 ? src1->nb[1] : 1;
+    const int64_t nb12 = src1 ? src1->nb[2] : 1;
+    const int64_t nb13 = src1 ? src1->nb[3] : 1;
+
+    const int64_t ne12 = src1 ? src1->ne[2] : 1;
+    const int64_t ne13 = src1 ? src1->ne[3] : 1;
+
+    const uint32_t n_head      = src0->ne[2];
+    const uint32_t n_head_log2 = 1u << (uint32_t) floorf(log2f((float) n_head));
+
+    const float m0 = powf(2.0f, -(max_bias       ) / n_head_log2);
+    const float m1 = powf(2.0f, -(max_bias / 2.0f) / n_head_log2);
+
+    soft_max_params params = {};
+    params.nheads = src0->ne[2];
+    params.n_head_log2 = n_head_log2;
+    params.ncols = ne00;
+    params.nrows_x = nrows_x;
+    params.nrows_y = nrows_y;
+    params.ne00 = src0->ne[0];
+    params.ne01 = src0->ne[1];
+    params.ne02 = src0->ne[2];
+    params.ne03 = src0->ne[3];
+    params.nb11 = nb11;
+    params.nb12 = nb12;
+    params.nb13 = nb13;
+    params.ne12 = ne12;
+    params.ne13 = ne13;
+    params.scale = scale;
+    params.max_bias = max_bias;
+    params.m0 = m0;
+    params.m1 = m1;
+
+    // float params[4];
+    // memcpy(params, dst->op_params, sizeof(params));
 
     const bool use_f16 = (src1 && src1->type == GGML_TYPE_F16);
     //printf("%s: %g, %g, %g, %g, %p, %d\n", __func__, params[0], params[1], params[2], params[3], (const void *)src1, use_f16);
@@ -365,11 +408,11 @@ void ggml_cuda_op_soft_cap_max(ggml_backend_cuda_context & ctx, ggml_tensor * ds
     if (use_f16) {
         const half * src1_dd = (const half *)src1_d;
 
-        soft_max_f32_cuda(src0_d, src1_dd, dst_d, ne00, nrows_x, nrows_y, params[0], params[1], params[2], params[3], true, stream);
+        soft_max_f32_cuda(src0_d, src1_dd, dst_d, params, 0, 0, true, stream);
     } else {
         const float * src1_dd = (const float *)src1_d;
 
-        soft_max_f32_cuda(src0_d, src1_dd, dst_d, ne00, nrows_x, nrows_y, params[0], params[1], params[2], params[3], true, stream);
+        soft_max_f32_cuda(src0_d, src1_dd, dst_d, params, 0, 0, true, stream);
     }
 }
 
